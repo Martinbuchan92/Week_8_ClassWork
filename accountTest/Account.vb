9c76534850fb76655ca4c5abe3238cf1585967a6
Martin Buchan <martinbuchan92@gmail.com>
2017-09-12 14:12:04 +1000

    Add project files.

---
+++ accountTest/Account.vb
@@ -0,0 +1,50 @@
+﻿Public Class Account
+    Private m_accountName As String
+    Private m_accountBalance As Double
+    Private m_accountNumber As Integer
+    Private Shared acctNumber As Integer = 1
+    Private Shared ReadOnly minBalance As Double = 50.0
+
+    Public Sub New()
+        Me.New("Blank", minBalance)
+    End Sub
+
+    Public Sub New(ByVal accountName As String, accountBalance As Double)
+        m_accountName = accountName
+        m_accountBalance = accountBalance
+        m_accountNumber = acctNumber
+        acctNumber += 1
+    End Sub
+
+    Public Sub Withdraw(ByVal amount As Double)
+        m_accountBalance += amount
+    End Sub
+
+    Public Sub AddInterest(ByVal rate As Double)
+        m_accountBalance = rate * AccountBalance
+    End Sub
+
+
+    Public Overrides Function ToString() As String
+        Return m_accountName + ", " + m_accountBalance.ToString() + ", " + m_accountNumber.ToString
+    End Function
+
+    Public Property AccountName() As String
+        Get
+            Return m_accountName
+        End Get
+        Set(value As String)
+            m_accountName = value
+        End Set
+    End Property
+
+    Public Property AccountBalance As Double
+        Get
+            Return m_accountBalance
+        End Get
+        Set(value As Double)
+            m_accountBalance = value
+        End Set
+    End Property
+
+End Class
---
+++ accountTest/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+    Dim test1 As New Account()
+    Dim test2 As New Account("Martin", 75)
+
+    Sub Main()
+        Console.Write(test1.ToString)
+        Console.Read()
+
+
+    End Sub
+
+End Module
